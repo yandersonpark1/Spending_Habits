51d2ae270924d03afa96dd260b802e5de28c833b
yandersonpark1 <everydayap3@gmail.com>
2025-09-18 00:03:38 -0400

    initial: spring boot project

---
+++ src/test/java/com/example/playlistcustomizer/PlaylistCustomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playlistcustomizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistCustomizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
